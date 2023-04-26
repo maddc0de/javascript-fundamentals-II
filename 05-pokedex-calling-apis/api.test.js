416c334fbb10f3017ba7106898c99cc23b432664
Madelane <madelanedaz@gmail.com>
2023-04-26 15:38:56 +0100

    create test file for fetchPokemon method

---
+++ 05-pokedex-calling-apis/api.test.js
@@ -0,0 +1,20 @@
+const fetchPokemon = require('./api')
+
+// describe('fetchPokemon', () => {
+//   it('returns a promise of the useful pokemon data', (done) => {
+//     fetchPokemon('charizard')
+//       .then((pokemon) => {
+//         expect(pokemon.id).toEqual(6);
+//         done();
+//       });
+//   });
+// });
+
+describe('fetchPokemon', () => {
+  it('Can fetch the data for Charizard', async () => {
+    const pokemon = await fetchPokemon('charizard')
+
+    // console.log(fetchPokemon('charizard'));  -> prints Promise { <pending> }
+    expect(pokemon.id).toEqual(6);
+  });
+});
